3754d1d2bb38cea5deeea4ef233df8942daa2a66
daniel.caamano <daniel.caamano@amodosoluciones.com>
2026-02-02 07:50:20 +0000

    favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,11 +1,29 @@
+@php
+    static $appIconSource;
+    static $appIconInitialized = false;
+
+    if (! $appIconInitialized) {
+        $logoPath = storage_path('app/logo-noBG.png');
+        $appIconSource = file_exists($logoPath)
+            ? 'data:image/png;base64,'.base64_encode(file_get_contents($logoPath))
+            : null;
+        $appIconInitialized = true;
+    }
+@endphp
+
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+@if ($appIconSource)
+    <link rel="icon" href="{{ $appIconSource }}" sizes="any" type="image/png">
+    <link rel="apple-touch-icon" href="{{ $appIconSource }}">
+@else
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+@endif
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
